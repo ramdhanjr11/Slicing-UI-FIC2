fb2ae4888727997f58e30e9e1f5d67552d89ffad
Muramsyah <ramdhanjr690@gmail.com>
2023-02-24 16:41:50 +0700

    initial empty flutter app

---
+++ slicing_ui_pro1/README.md
@@ -0,0 +1,3 @@
+# slicing_ui_pro1
+
+This app is slicing of UI PRO 1 FIC 2
---
+++ slicing_ui_pro1/android/app/src/main/kotlin/com/example/slicing_ui_pro1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.slicing_ui_pro1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
