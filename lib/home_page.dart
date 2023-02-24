90d47173e593af6523fcf63120d8b43dba7e6831
Muramsyah <ramdhanjr690@gmail.com>
2023-02-24 17:13:38 +0700

    ui: add empty ui for home page

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static const routeName = '/home_page';
+
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        centerTitle: false,
+      ),
+      body: const Center(
+        child: Text('I will design in this page first'),
+      ),
+    );
+  }
+}
