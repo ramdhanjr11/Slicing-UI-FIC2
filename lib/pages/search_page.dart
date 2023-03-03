ead333277095e519b585da35b120e6ad45de80df
Muramsyah <ramdhanjr690@gmail.com>
2023-03-03 17:27:14 +0700

    ui(search page): add title content and gridview image for travel items

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:ui_pro_2/models/travel_model.dart';
+import 'package:ui_pro_2/widgets/search_item_travel.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Search Page'),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Top Tours',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineMedium!
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'For your request',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                ],
+              ),
+            ),
+            MasonryGridView.count(
+              crossAxisCount: 2,
+              itemCount: travelItems.length,
+              mainAxisSpacing: 40,
+              crossAxisSpacing: 15,
+              padding: const EdgeInsets.all(24),
+              shrinkWrap: true,
+              primary: false,
+              itemBuilder: (context, index) {
+                return SearchItemTravel(
+                  index: index,
+                  travelItem: travelItems[index],
+                );
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/search_item_travel.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:ui_pro_2/models/travel_model.dart';
+
+class SearchItemTravel extends StatelessWidget {
+  final int index;
+  final TravelModel travelItem;
+
+  const SearchItemTravel({
+    super.key,
+    required this.index,
+    required this.travelItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, index.isOdd ? 50 : 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  width: 160,
+                  height: 220,
+                  fit: BoxFit.cover,
+                  travelItem.imageUrl,
+                ),
+              ),
+              const Positioned(
+                top: 12,
+                right: 12,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.favorite, size: 18),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Text(
+            travelItem.travelName,
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 4),
+          Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: r'$459.00',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: '/${travelItem.longTrip}',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall!
+                      .copyWith(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
